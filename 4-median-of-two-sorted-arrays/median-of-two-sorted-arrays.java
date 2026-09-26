2cca38e01c70b9c53cbdc2863287c77c59767ccb
OC2427 <om.chandrakar2427@gmail.com>
2026-09-26 18:47:28 +0530

    Time: 26 ms (13.41%) | Memory: 48.8 MB (51.84%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -4,19 +4,16 @@ class Solution {
         System.arraycopy(nums1, 0, nums3, 0, nums1.length);
         System.arraycopy(nums2, 0, nums3, nums1.length, nums2.length);
         int len = nums3.length, temp;
-        if (len == 1) return (double)nums3[0];
-        else{
-            for (int i = 0; i < len - 1; i++){
-                for (int j = i + 1; j < len; j++){
-                    if (nums3[i] > nums3[j]){
-                        temp = nums3[i];
-                        nums3[i] = nums3[j];
-                        nums3[j] = temp;
-                    }
+        for (int i = 0; i < len - 1; i++){
+            for (int j = i + 1; j < len; j++){
+                if (nums3[i] > nums3[j]){
+                    temp = nums3[i];
+                    nums3[i] = nums3[j];
+                    nums3[j] = temp;
                 }
             }
-            if (len%2 != 0) return nums3[len>>1];
-            return (nums3[len>>1] + nums3[(len>>1)-1]) / 2.0;
         }
+        if (len%2 != 0) return nums3[len>>1];
+        return (nums3[len>>1] + nums3[(len>>1)-1]) / 2.0;
     }
 }
